1fd6fb1d9afd9fac6763c2b5852a0e5cb9e276d6
Vedant Naik <vedantnaik19@gmail.com>
2021-06-04 13:22:16 +0530

    exercises api

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/exercises', (req, res) => {
-    res.json({ data: 'Hello World!' });
+    res.json({ data: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.' });
 })
 
 app.listen(process.env.PORT || 8080);
