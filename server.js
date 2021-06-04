50508e0e118e650aea95a8158179ec1587222480
Vedant Naik <vedantnaik19@gmail.com>
2021-06-04 13:17:03 +0530

    exercises api

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/chat-test'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/chat-test/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/chat-test/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+app.get('/exercises', (req, res) => {
+    res.json({ data: 'Hello World!' });
+})
+
 app.listen(process.env.PORT || 8080);
